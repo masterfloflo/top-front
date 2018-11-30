f457c069f4ca9ea13ca793d1aa5cb31329c03e7d
masterfloflo <43879013+masterfloflo@users.noreply.github.com>
2018-11-30 15:22:44 +0100

    testtest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,11 +7,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, NgbModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
 export class YourAppModule {
 }
