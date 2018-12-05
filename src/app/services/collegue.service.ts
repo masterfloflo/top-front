f9e827921d90b73a54b45ca1e96f242c3f9f004e
masterfloflo <43879013+masterfloflo@users.noreply.github.com>
2018-12-05 12:06:26 +0100

    az

--- src/app/services/collegue.service.ts
+++ src/app/services/collegue.service.ts
@@ -2,6 +2,15 @@ import { Injectable } from '@angular/core';
 import {environment} from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Collegue, Avis } from '../models';
+import { HttpHeaders } from '@angular/common/http';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'my-auth-token'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -15,19 +24,18 @@ export class CollegueService {
     return this._http.get(environment.backendUrl +'/collegue').toPromise()
     .then((tabColServeur:any[]) => tabColServeur.map(coco => new Collegue(coco.score, coco.pseudo, [coco.imageUrl])))
   }
-/*
+
+
   donnerUnAvis(unCollegue:Collegue, avis:Avis):Promise<Collegue>  {
-    // TODO Aimer ou Détester un collègue côté serveur
-    return this.http.post()
+    // Aimer ou Détester un collègue côté serveur
+    
+    return this._http.patch(environment.backendUrl +'/collegue', {"action":avis.toString}, httpOptions)
+    .toPromise().then((c:Collegue) => c)   
+  
+    
+    
+
+
   }
-
-
-
-la méthode permet de TOUT faire : request
-
-par exemple .post (ICI ADRESSE URL  , CORPS DE LA REQUETE, OPTIONS POUR SPECIFIER LES ENTETES)
-
-*/
-
 }
 
